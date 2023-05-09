fbf097f1215a465af55684652cd7e6bdc2a27306
Charlie <cdb8987@gmail.com>
2023-05-09 14:45:48 -0400

    separated message feed into a new component outside of mainpage.js

--- chat-app-front-end/src/Components/MainPage.js
+++ chat-app-front-end/src/Components/MainPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import UserData from './UserData';
+import MessageFeed from './MessageFeed';
 
 function MainPage(props){
     
@@ -86,25 +87,18 @@ function MainPage(props){
         return ()=>{clearInterval(interval)}
     }, [])
     
- 
-    // let UserData = props.activeusers ? (
+    // let MessageData = (
     //     <div>
-    //         {props.activeUsers.map(item=>(<p>🟢{item}</p>))}
+    //         {props.messageFeed.map(item=> (
+    //         <div><strong>{item.name}</strong> {item.time}   
+    //         <div>
+    //             <p style={{border:'gray', borderStyle:'solid', borderRadius:'5px'}}>{item.message}</p>
+    //             </div>
+    //         </div>
+    //         ))}
     //     </div>
-    // ) : null
-    
-    let MessageData = (
-        <div>
-            {props.messageFeed.map(item=> (
-            <div><strong>{item.name}</strong> {item.time}   
-            <div>
-                <p style={{border:'gray', borderStyle:'solid', borderRadius:'5px'}}>{item.message}</p>
-                </div>
-            </div>
-            ))}
-        </div>
         
-    )
+    // )
     let Channellist = (
         <div>
             {props.channels.map(item=> (
@@ -125,14 +119,12 @@ function MainPage(props){
                             <button className="btn btn-outline-primary">Friends</button>
                             <button className="btn btn-outline-primary">Channels</button>                       
                         </div>
-                        {/* <div className="userdata"> <UserData activeusers={props.activeusers}/>
-                        </div> */}
                         <UserData activeUsers={props.activeUsers}/>
                         <div classname="channellist">
                             {Channellist}
                         </div>
                     </div>
-                    <div className="messagefeed" style={{flexBasis: '66.66%'}}>
+                    {/* <div className="messagefeed" style={{flexBasis: '66.66%'}}>
                         <div  className="container p-3 my-3 border" style={{textAlign: 'center'}}>
                             <button className="btn btn-light">Feed</button>
                             <button className="btn btn-light">Direct Messages</button>
@@ -145,7 +137,8 @@ function MainPage(props){
                         </div>
                         
                         <div><button className="btn btn-primary" onClick={()=> {writeMessage(sendMessageText); setSendMessageText(''); updateActiveUsers();updateMessageFeed() }}  >Send Message</button><input style={{width:"100%"}}value={sendMessageText} onChange={(e) => setSendMessageText(e.target.value)} required></input></div>
-                    </div>
+                    </div> */}
+                    <MessageFeed messageFeed={props.messageFeed} logOut={logOut} writeMessage={writeMessage} sendMessageText={sendMessageText} setSendMessageText={setSendMessageText} updateActiveUsers={updateActiveUsers} updateMessageFeed={updateMessageFeed}/>
                     
                     
                 </div>
--- chat-app-front-end/src/Components/MessageFeed.js
+++ chat-app-front-end/src/Components/MessageFeed.js
@@ -1,6 +1,32 @@
-function MessageFeed(){
+function MessageFeed(props){
+    let MessageData = (
+        <div>
+            {props.messageFeed.map(item=> (
+            <div><strong>{item.name}</strong> {item.time}   
+            <div>
+                <p style={{border:'gray', borderStyle:'solid', borderRadius:'5px'}}>{item.message}</p>
+                </div>
+            </div>
+            ))}
+        </div>
+        
+    )
+    return (
+        <div className="messagefeed" style={{flexBasis: '66.66%'}}>
+                        <div  className="container p-3 my-3 border" style={{textAlign: 'center'}}>
+                            <button className="btn btn-light">Feed</button>
+                            <button className="btn btn-light">Direct Messages</button>
+                        
+                                <button className='btn btn-secondary' style={{float: 'right'}}onClick={()=> {props.logOut()}}>Log Out</button>
+                        </div>
 
-    return 
+                        <div style={{height: '70%', overflow:'auto'}}>
+                        {MessageData}
+                        </div>
+                        
+                        <div><button className="btn btn-primary" onClick={()=> {props.writeMessage(props.sendMessageText); props.setSendMessageText(''); props.updateActiveUsers(); props.updateMessageFeed() }}  >Send Message</button><input style={{width:"100%"}}value={props.sendMessageText} onChange={(e) => props.setSendMessageText(e.target.value)} required></input></div>
+                    </div>
+    )
 }
 
 export default MessageFeed
