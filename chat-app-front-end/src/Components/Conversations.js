c3b7e4cd78ce140c76bc768d6679cb6bb4782740
Charlie <cdb8987@gmail.com>
2023-05-10 15:05:57 -0400

    front end can now select a username and write a direct message to that user

--- chat-app-front-end/src/Components/Conversations.js
+++ chat-app-front-end/src/Components/Conversations.js
@@ -1,11 +1,18 @@
+import { useState } from "react"
+
 
 function Conversations(){
+    let [value, SetValue] = useState('')
 
+    const handleChange = (event)=>{
+        SetValue(event.target.value);
+        console.log(value)
+    }
     return (
         <form onSubmit={()=>{console.log('Username Entered')}}>
             <label>Recipient:</label>
-            <input></input>
-            <input type="submit"></input>
+            <input onChange={handleChange}></input>
+            <input type="submit" onClick={()=>{sessionStorage.setItem('RecipientUsername', value )}}></input>
         </form>
         
     )
