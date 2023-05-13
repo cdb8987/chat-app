f7d7d2fb2fc8a722fdf64d23a02a98e463136ef9
Charlie <cdb8987@gmail.com>
2023-05-13 10:40:30 -0400

    changed messagefeedtitle so that it updates based on the direct message partner

--- chat-app-front-end/src/Components/MessageFeedTitle.js
+++ chat-app-front-end/src/Components/MessageFeedTitle.js
@@ -1,6 +1,6 @@
 export default function MessageFeedTitle(){
     let TitleContent;
-    TitleContent = (sessionStorage.getItem('LeftContainerComponentSelect')==='Channels') ?`#${sessionStorage.getItem('ChannelName')}` : `Message History`
+    TitleContent = (sessionStorage.getItem('LeftContainerComponentSelect')==='Channels') ?`#${sessionStorage.getItem('ChannelName')}` : `Message History: ${sessionStorage.getItem('FriendUsername')}`
 
     return <p className="MessageFeedTitle">{TitleContent}</p>
 }
